9e4231a180f10ec86eaf853be855308d9f95ec77
YukiMaitani <fightzoi1997@gmail.com>
2022-05-11 12:44:23 +0900

    Initial Commit

---
+++ Kadai1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kadai1
+//
+//  Created by 米谷裕輝 on 2022/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
