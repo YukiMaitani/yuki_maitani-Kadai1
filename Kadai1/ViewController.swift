65c21b02efba748303babde2a31ed927fa241a86
YukiMaitani <fightzoi1997@gmail.com>
2022-05-11 13:53:47 +0900

    課題１の最初の提出

--- Kadai1/ViewController.swift
+++ Kadai1/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textFieldCollection: [UITextField]!
+    @IBOutlet weak var sumResultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    //UITextField.text?にはデフォルトで""が入っているので強制アンラッピングして、""をInt型にするとnilになるのでreduceで足すときにはnilの場合は0になるようにしている
+    @IBAction func tappedSumButtom(_ sender: Any) {
+        sumResultLabel.text = String(textFieldCollection.map{Int($0.text!)}.reduce(0){$0 + ($1 ?? 0)})
+    }
+    
 }
 
