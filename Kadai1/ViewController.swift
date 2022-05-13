5e38459bb310a8782a352c452940922d4cf2bb0f
akio0911 <akio0911@gmail.com>
2022-05-13 12:18:47 +0900

    feedback

--- Kadai1/ViewController.swift
+++ Kadai1/ViewController.swift
@@ -14,11 +14,11 @@ class ViewController: UIViewController {
 
     //UITextField.text?にはデフォルトで""が入っているので強制アンラッピングして、""をInt型にするとnilになるのでreduceで足すときにはnilの場合は0になるようにしている
     @IBAction func tappedSumButtom(_ sender: Any) {
-        let total = textFieldCollection
-            .map { Int($0.text!) ?? 0 }
-            .reduce(0, +)
-        
-        sumResultLabel.text = String(total)
+        sumResultLabel.text = String(
+            textFieldCollection
+                .map { Int($0.text!) ?? 0 }
+                .reduce(0, +)
+        )
     }
     
 }
